f1c107014e7bb2f52c40dc9e3058fe7025e5a966
AminAvan <amin.avan@ontariotechu.net>
2025-02-19 12:17:12 -0500

    editing the main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import collections
 import time
 import numpy as np
-# import tensorflow as tf
-# import tensorflow_federated as tff
 import pandas as pd
 from datetime import datetime, date
-import pdb
-import math
 import itertools
 import warnings
 import csv
-import psutil
 import psutil
 warnings.simplefilter("ignore")
 
@@ -44,13 +38,12 @@ def state_generator(user_num, server_num):
 
   # Generate all possible permutations of the matrix
   permutations = list(itertools.product(*([CELL_VALUES]*user_num*server_num)))
-  # print(permutations)
+
   # Filter out permutations that violate the constraint
   valid_permutations = []
   counter = 0
   row_checker = list()
   for perm in permutations:
-      # print("perm", perm)
       matrix = [list(perm[i:i+server_num]) for i in range(0, user_num*server_num, server_num)]
       # pdb.set_trace()
       matrix = np.reshape(np.array(matrix),(user_num,server_num))
@@ -64,15 +57,10 @@ def state_generator(user_num, server_num):
       col_sum = np.sum(matrix, axis= 0)
       flag_col = all(col_sum)
 
-      # if all(sum <= 1 for sum in column_sums) and (flag_row == True and flag_col == True) or np.sum(matrix) == 0 :
+
       if all(sum <= 1 for sum in column_sums) and (flag_row == True) or np.sum(matrix) == 0 :
           state_list.append(matrix)
-          # print("matrix", matrix)
           counter = counter + 1
-          #print("valid_matrix \n", matrix)
-  # print("================================")
-  # print("counter", counter)
-  # print("================================")
   return state_list, counter
 
 
@@ -81,18 +69,14 @@ def reward_function(current_state, next_state, num_tasks, num_resources, task_da
   total_reward = 0
   active_server_num = np.sum(next_state)
   index_tasks = list(np.where(current_state == 1)[0])
-  # print("current_state", current_state)
-  # print("next_state", next_state)
   y = False
   for i in index_tasks:
       task_slice = task_dataframe.loc[i].copy()
       task_slice["exection_time"] -= 1
       task_dataframe.loc[i] = task_slice
-      # task_dataframe.loc[i]["exection_time"] = task_dataframe.loc[i]["exection_time"] -1
       # pdb.set_trace()
-  # print("\/\/\/\/\/\/")
   # print("task_dataframe in reward:", task_dataframe)
-  # print("\/\/\/\/\/\/")
+
 
   execution_time_list = list(task_dataframe.loc[index_tasks]["exection_time"])
   all_greater_than_0 = all(x >= 0 for x in execution_time_list)
@@ -104,26 +88,13 @@ def reward_function(current_state, next_state, num_tasks, num_resources, task_da
     #print("workload scheduled, here is the dataframe:", task_dataframe)
   if active_server_num == 0:
     total_reward = -((num_resources*(num_tasks*num_resources))**num_tasks)
-    done = True #amin
+    done = True
   else:
-    #print('The CPU usage is: ', psutil.cpu_percent(1)) # Calling psutil.cpu_precent() for 4 seconds
-    # for tracking the cpu & RAM usage
-    #rec_cpu = 0
-    #rec_cpu = psutil.cpu_percent(1)
-    #if (rec_cpu > 10.0):
-    # with open('/content/drive/MyDrive/network_comp_prjct/QED/QED_RAM_rec_96tasks.csv', mode='a', newline='') as f:
-    #   # create the csv writer
-    #   writer = csv.writer(f)
-    #   # write a row to the csv file
-    #   RAM_REC = psutil.virtual_memory()[2]
-    #   writer.writerow([RAM_REC])
-
     total_reward = total_reward + (active_server_num*(num_tasks*num_resources))
     if ((next_state == current_state) & (all_greater_than_0 == True )).any():
           total_reward = total_reward + (num_resources*(num_tasks*num_resources))
     elif ((next_state == current_state) & (all_greater_than_0 == False )).any():
           total_reward = total_reward - (num_resources*(num_tasks*num_resources))
-  #elif -1 in task_dataframe["exection_time"]:
   if any(task_dataframe["exection_time"] < 0):
     total_reward = -((num_resources*(num_tasks*num_resources))**num_tasks)
     # if there is any (-1) in the execution time of dataframe, then it should consider as a GAMEOVER for the learning
@@ -143,7 +114,6 @@ def q_learning(user_num, num_resources, top_not_excuted_tasks):
     episode = 0
     while(episode_brocker != True):
         episode = episode +1
-    # for episode in range(num_episodes):
         task_dataframe = top_not_excuted_tasks.copy()
         # pdb.set_trace()
         # print("episode =>", episode)
@@ -176,21 +146,12 @@ def q_learning(user_num, num_resources, top_not_excuted_tasks):
               q_table[:,0] = r
             current_state = next_state
 
-
-            # Check if the episode is done
-            #if sum(df["exection_time"]) == 0
-            # if 8 or total_reward < -deadline:
-            #     done = True
-
             if (all(x == 0 for x in task_dataframe["exection_time"])):
                 done = True
         # print("Last dataframe:", task_dataframe)
         # print("Episode:", episode, "Total Reward:", total_reward)
         # print('==================================================')
-        # print('\n')
     return episode_brocker
-
-
 
 # Determining the number of times that PBQ performs scheduling is useful for executing multiple runs
 # to compute the average runtime and RAM usage.
@@ -231,12 +192,10 @@ for j in range(1, 2):
       #print("end:", d2,"at", current_time)
   total_time = 0
   total_time=time.time()-start_time
+  # runtime for each round of PBQ on a normal machine in scenario
+  #   with one edge server and 24, 48, or 96 tasks, the runtime is 1054, 2053, or 3186 seconds, respectively.
+  #   with two edge servers and 24, 48, or 96 tasks, the runtime is 82, 160, or 317 seconds, respectively.
   print(f"total time of {j}th run is: {total_time}")
   # Get and print the memory usage
   memory_usage = get_memory_usage()
   print(f"Memory Usage: {memory_usage:.2f} MB")
-
-
-# Get and print the memory usage
-memory_usage = get_memory_usage()
-print(f"Memory Usage: {memory_usage:.2f} MB")
